021d9bb081250816856ba641fd0495d349efb8cb
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-06-08 16:23:03 +0300

    add 28 lessons

--- lesson 28/shmoment/index.js
+++ lesson 28/shmoment/index.js
@@ -6,36 +6,55 @@
 export const shmoment = (date) => {
   let initialDate = new Date(date);
 
-  const years = initialDate.getFullYear();
-  const month = initialDate.getMonth();
-  const days = initialDate.getDate();
-  const hours = initialDate.getHours();
-  const minutes = initialDate.getMinutes();
-  const seconds = initialDate.getSeconds();
-  const milliseconds = initialDate.getMilliseconds();
+  let years = initialDate.getFullYear();
+  let month = initialDate.getMonth();
+  let days = initialDate.getDate();
+  let hours = initialDate.getHours();
+  let minutes = initialDate.getMinutes();
+  let seconds = initialDate.getSeconds();
+  let milliseconds = initialDate.getMilliseconds();
 
-  // const checkTimeInterval = (timeInterval, num) => {
-  //   if (timeInterval === 'years') {
-  //     return new Date(initialDate).setFullYear(years + num);
-  //   }
-  //   if (timeInterval === 'month') {
-  //     return new Date(initialDate).setMonth(month + num);
-  //   }
-  // } 
+  const checkTimeIntervalAdd = (timeInterval, num, operator) => {
+
+    if (timeInterval === 'years') {
+      initialDate = new Date(initialDate).setFullYear(operator ? years + num : years - num);
+      years = new Date(initialDate).getFullYear();
+    }
+    if (timeInterval === 'months') {
+      initialDate = new Date(initialDate).setMonth(operator ? month + num : month - num);
+      month = new Date(initialDate).getMonth();
+    }
+    if (timeInterval === 'days') {
+      initialDate = new Date(initialDate).setDate(operator ? days + num : days - num);
+      days = new Date(initialDate).getDate();
+    }
+    if (timeInterval === 'hours') {
+      initialDate = new Date(initialDate).setHours(operator ? hours + num : hours - num);
+      hours = new Date(initialDate).getHours();
+    }
+    if (timeInterval === 'minutes') {
+      initialDate = new Date(initialDate).setMinutes(operator ? minutes + num : minutes - num);
+      minutes = new Date(initialDate).getMinutes();
+    }
+    if (timeInterval === 'seconds') {
+      initialDate = new Date(initialDate).setSeconds(operator ? seconds + num : seconds - num);
+      seconds = new Date(initialDate).getSeconds();
+    }
+    if (timeInterval === 'milliseconds') {
+      initialDate = new Date(initialDate).setMilliseconds(operator ? milliseconds + num : milliseconds - num);
+      milliseconds = new Date(initialDate).getMilliseconds();
+    }
+  } 
 
   const calculatorDate = {
       add(timeInterval, num) {
-        if (timeInterval === 'years') {
-          initialDate = new Date(initialDate).setFullYear(years + num);
-        }
+        checkTimeIntervalAdd(timeInterval, num, true);
         return this;
       },
 
       subtract(timeInterval, num) {
-        if (timeInterval === 'years') {
-          initialDate = new Date(initialDate).setFullYear(years + num);
-        }
-        return this
+        checkTimeIntervalAdd(timeInterval, num, false);
+        return this;
       },
 
       result() {
@@ -46,12 +65,5 @@ export const shmoment = (date) => {
   return calculatorDate;
 }
 
-console.log(shmoment(new Date(2021, 5, 7, 12, 13, 45)).add('years', 1).subtract('years', 1).result());
+console.log(shmoment(new Date(2021, 5, 7, 12, 13, 45)).add('years', 1).add('month', 2).add('years', 1).subtract('years', 1).subtract('minutes', 3).result());
 
-
-
-const dayOfWeek = (date, days) => {
-  const day = new Date(date).getDate();
-  const dayOfFuture = new Date(date).setDate(day + days);
-  return  weekDays[new Date(dayOfFuture).getDay()];
-} 
