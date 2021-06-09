714ce0d7f837914d68d4114927129a3cf4f37e05
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-06-09 19:22:36 +0300

    add

--- lesson30-promise/task4-promisification/index.js
+++ lesson30-promise/task4-promisification/index.js
@@ -1,25 +1,43 @@
+// export const addImageV2 = (url) => {
+//   const p = new Promise((resolve, reject) => {
+//     const addImage = (callback) => {
+//       const img = document.createElement('img');
+//       img.setAttribute('alt', 'User avatar');
+//       img.src = url;
+    
+//       const pageElem = document.querySelector('.page');
+//       pageElem.append(img);
+    
+//       const onImageLoaded = () => {
+//         const { width, height } = img;
+//         callback(null, { width, height });
+//       };
+    
+//       const onImageLoadError = () => callback('Image load failed');
+    
+//       img.addEventListener('load', onImageLoaded);
+    
+//       img.addEventListener('error', onImageLoadError);
+//     };
+    
+//     const onImageLoaded = (error, data) => {
+//       if (error) {
+//         reject(error);
+//       }
+//       resolve(data);
+//     }
+
+//     addImage(onImageLoaded);
+//   })
+//   return p;
+// }
+
+// ==========================================================
+
+import { addImage } from './addImage.js';
+
 export const addImageV2 = (url) => {
   const p = new Promise((resolve, reject) => {
-    const addImage = (callback) => {
-      const img = document.createElement('img');
-      img.setAttribute('alt', 'User avatar');
-      img.src = url;
-    
-      const pageElem = document.querySelector('.page');
-      pageElem.append(img);
-    
-      const onImageLoaded = () => {
-        const { width, height } = img;
-        callback(null, { width, height });
-      };
-    
-      const onImageLoadError = () => callback('Image load failed');
-    
-      img.addEventListener('load', onImageLoaded);
-    
-      img.addEventListener('error', onImageLoadError);
-    };
-    
     const onImageLoaded = (error, data) => {
       if (error) {
         reject(error);
@@ -27,12 +45,12 @@ export const addImageV2 = (url) => {
       resolve(data);
     }
 
-    addImage(onImageLoaded);
+    addImage(url, onImageLoaded);
   })
   return p;
 }
 
-// ==========================================================
+
 const imgSrc = ("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg");
 
 
